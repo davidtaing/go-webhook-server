cde9d76292b9f1e84a27d560cf443748405f6b6a
David Taing <dave@teeang.net>
2024-02-06 00:09:53 +1100

    refactor: Webhook handler to standalone fn

--- main.go
+++ main.go
@@ -6,15 +6,17 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
+func WebhookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 
-		// Handle the POST request here
-		fmt.Println("Webhook received!")
-	})
+	// Handle the POST request here
+	fmt.Println("Webhook received!")
+}
+
+func main() {
+	http.HandleFunc("/webhook", WebhookHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
