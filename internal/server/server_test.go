357b2dc4b11c6d14ffea20185945bcfe64ef2f07
David Taing <dave@teeang.net>
2024-02-06 22:03:15 +1100

    test: WebhookHandler returning 200 for duplicated event

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -34,4 +36,40 @@ func TestWebhookHandler_MethodNotAllowed(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestWebhookHandler_HandleDuplicateEvent(t *testing.T) {
+	event := WebhookEvent{
+		ID:    1,
+		Event: "test",
+	}
+
+	eventJSON, err := json.Marshal(event)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	eventBuffer := bytes.NewBuffer(eventJSON)
+
+	_, err = http.NewRequest(http.MethodPost, "/webhook", eventBuffer)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, "/webhook", eventBuffer)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(WebhookHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	// expect a 200 OK status code, as non OK statuses will cause the duplicate event to be retried by the sender
+	t.Run("returns 200 OK upon duplicated event", func(t *testing.T) {
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+	})
 }
