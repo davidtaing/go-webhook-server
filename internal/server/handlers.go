1dc5864a0c4d800e1348fb6a4aa05031c628bdb6
David Taing <dave@teeang.net>
2024-02-08 13:46:58 +1100

    WIP: add planning comments
    The main aim for this project is to learn and create a portfolio piece.
    
    Normally this would be cleaned up (via interactive rebase or some other
    way). But I am deliberately "showing my working out".

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -11,6 +11,15 @@ func (s *server) handleWebhook() http.HandlerFunc {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
 
+		// check if the webhook is already in DB
+		// ✅ - if yes return 200 OK.
+
+		// need to return 200 OK, or the request will be treated as a failure
+		// and will be retried/resent by the sender
+
+		// create if webhook doesn't exist
+
+		// return creation result as status
 		fmt.Println("Webhook received!")
 	}
 }
