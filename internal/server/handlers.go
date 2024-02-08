698e410ac84200792ca5343b588504903976e804
David Taing <dave@teeang.net>
2024-02-08 21:53:21 +1100

    fix: handleWebhook not existing on invalid http method

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -19,6 +19,7 @@ func (s *server) handleWebhook() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
 		}
 
 		var (
